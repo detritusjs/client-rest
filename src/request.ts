9b87875589d352a00ee9c0947281744efa40810d
Timo <30553356+y21@users.noreply.github.com>
2020-01-11 11:57:59 +0100

    typeof null === "object"

--- src/request.ts
+++ src/request.ts
@@ -246,7 +246,7 @@ export class RestRequest {
 
     const data = await response.body();
     if (!response.ok) {
-      if (typeof(data) === 'object') {
+      if (typeof(data) === 'object' && data !== null) {
         if (
           (this.client.restClient.baseUrl instanceof URL) &&
           (this.client.restClient.baseUrl.host === this.request.url.host)
