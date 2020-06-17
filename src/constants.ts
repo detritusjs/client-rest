4eeea98056fefc3c08154415fbbd40ead359e8c3
Dan <31395415+cakedan@users.noreply.github.com>
2020-06-16 18:04:02 -0600

    Update detritus-rest requirement

--- src/constants.ts
+++ src/constants.ts
@@ -6,7 +6,7 @@ export { HTTPMethods };
 
 export const Package = Object.freeze({
   URL: 'https://github.com/detritusjs/client-rest',
-  VERSION: '0.8.0.beta.0',
+  VERSION: '0.8.0-beta.1',
 });
 
 
